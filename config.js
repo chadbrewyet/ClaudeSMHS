52aa7fce89b114908e0dc18d4d29956d2dde3a31
chadbrewyet <chadbrewyet@gmail.com>
2026-02-18 09:24:38 -0600

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Get your Client ID from: https://developer.spotify.com/dashboard
 const SPOTIFY_CONFIG = {
     CLIENT_ID: '6fcf4eda70c44b08878b97d61c185764', // Replace with your actual Client ID
-    REDIRECT_URI: 'https://chadbrewyet.github.io/ClaudeSMHS', // Change this to match your setup
+    REDIRECT_URI: 'https://chadbrewyet.github.io/ClaudeSMHS/', // Change this to match your setup
     SCOPES: [
         'streaming',
         'user-read-email',
